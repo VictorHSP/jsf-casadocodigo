e1dbcdff8d151f30b1ccfe418c2ff929140e3dcc
Victor Hugo <victor.hugo.s@hotmail.com>
2025-04-29 19:04:27 -0300

    fix: encrypt password with bcrypt

--- src/main/resources/db/changelog/changes/changelog.insert-user.sql
+++ src/main/resources/db/changelog/changes/changelog.insert-user.sql
@@ -2,5 +2,5 @@
 
 --changeset victor.hugo:changelog.insert-user
 INSERT INTO public."tb_user" (NAME, EMAIL, USERNAME, PASSWORD, LOCALE, LAST_UPDATE_AT, ACTIVE)
-VALUES ('Victor Hugo', 'victor@gmail.com', 'victorhsp', '123', 'pt_BR', now(), true);
+VALUES ('Victor Hugo', 'victor@gmail.com', 'victorhsp', '$2a$12$hb9CszkLpNp6Jw3wdNJiP.QUokSozQlPNf09kD8q26IMRpdV8Tplm', 'pt_BR', now(), true);
 --rollback DELETE FROM public.user WHERE email = 'victor@gmail.com';
